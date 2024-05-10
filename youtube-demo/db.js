e90ffe1b89904bd98af99e76c6392a62511f806d
jellysoo97 <jellysoo97@gmail.com>
2024-05-10 23:57:11 +0900

    [programmers] 240509 assignment

---
+++ youtube-demo/db.js
@@ -0,0 +1,20 @@
+// get client
+const mysql = require("mysql2");
+
+// create connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  // timezone: 'Asia/Seoul',
+  database: "youtube",
+});
+
+// query
+// results: json array 형태의 쿼리 결과값
+// fields: users 테이블의 메타데이터
+connection.query("SELECT * FROM users", (err, results, fields) => {
+  const { id, email, name, created_at } = results[0];
+
+  console.log(id, email, name, created_at);
+});
