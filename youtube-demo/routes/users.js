99c7d22c2246fd8a577efa3090daabd5fa15628a
jellysoo97 <jellysoo97@gmail.com>
2024-05-13 00:07:01 +0900

    refactor: refactor users

--- youtube-demo/routes/users.js
+++ youtube-demo/routes/users.js
@@ -1,10 +1,7 @@
 const express = require("express");
-// router setting
 const router = express.Router();
-// db connection
 const conn = require("../db");
 
-// returns middleware that only parses json
 router.use(express.json());
 
 // ------------------------ api ------------------------
@@ -15,23 +12,19 @@ function isUserInputValid(user) {
 // 로그인
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
+  const sql = `SELECT * FROM users WHERE email = ? AND password = ?;`;
+  const values = [email, password];
 
-  conn.query(
-    `SELECT * FROM users WHERE email = ? AND password = ?;`,
-    [email, password],
-    (_, result) => {
-      // query 만족하는 데이터 없으면 result undefined 반환
-      if (result?.length) {
-        const { name } = result[0];
+  conn.query(sql, values, (_, result) => {
+    // query 만족하는 데이터 없으면 result undefined 반환
+    if (result?.length) {
+      const { name } = result[0];
 
-        res.status(200).json({ message: `${name}님 환영합니다!` });
-      } else {
-        res
-          .status(400)
-          .json({ message: "아이디나 비밀번호를 재입력해주세요." });
-      }
+      res.status(200).json({ message: `${name}님 환영합니다!` });
+    } else {
+      res.status(400).json({ message: "아이디나 비밀번호를 재입력해주세요." });
     }
-  );
+  });
 });
 
 // 회원가입
@@ -43,17 +36,15 @@ router.post("/join", (req, res) => {
   }
 
   const { email, name, password, phone } = req.body;
+  const sql = `INSERT INTO users (email, name, password, phone) VALUES (?, ?, ?, ?);`;
+  const values = [email, name, password, phone];
 
-  conn.query(
-    `INSERT INTO users (email, name, password, phone) VALUES (?, ?, ?, ?);`,
-    [email, name, password, phone],
-    (err) => {
-      // insert 쿼리는 result가 undefined
-      err
-        ? res.status(400).json(err)
-        : res.status(201).json({ message: `${name}님 환영합니다!` });
-    }
-  );
+  conn.query(sql, values, (err) => {
+    // insert 쿼리는 result가 undefined
+    err
+      ? res.status(400).json(err)
+      : res.status(201).json({ message: `${name}님 환영합니다!` });
+  });
 });
 
 router
@@ -61,10 +52,10 @@ router
   // 개별 회원 조회
   .get((req, res) => {
     const { email } = req.body;
+    const sql = `SELECT * FROM users WHERE email = ?;`;
+    const values = email;
 
-    // query1: `SELECT * FROM users WHERE email = "${email}";`,
-    // query2: `SELECT * FROM users WHERE email = ?;`, email,
-    conn.query(`SELECT * FROM users WHERE email = ?;`, email, (_, result) => {
+    conn.query(sql, values, (_, result) => {
       if (result?.length) {
         const { email, name } = result[0];
 
@@ -77,8 +68,10 @@ router
   // 개별 회원 탈퇴
   .delete((req, res) => {
     const { email } = req.body;
+    const sql = `DELETE FROM users WHERE email = ?;`;
+    const values = email;
 
-    conn.query(`DELETE FROM users WHERE email = ?;`, email, (_, result) => {
+    conn.query(sql, values, (_, result) => {
       result.affectedRows
         ? res.status(200).json({
             message: `회원 탈퇴 처리가 완료되었습니다.`,
