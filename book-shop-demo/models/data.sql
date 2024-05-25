b692f31e98698eb06eff0a388355f0e213cb86b0
jellysoo97 <jellysoo97@gmail.com>
2024-05-25 23:58:38 +0900

    [programmers] 240523 assignment

---
+++ book-shop-demo/models/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("어린왕자들", "종이책", 0, "어리다..", "많이 어리다..", "김어림", 100, "목차입니다.", 20000, "2019-01-01");
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("신데렐라들", "종이책", 1, "유리구두..", "투명한 유리구두..", "김구두", 100, "목차입니다.", 20000, "2023-12-01");
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("백설공주들", "종이책", 2, "사과..", "빨간 사과..", "김사과", 100, "목차입니다.", 20000, "2023-11-01");
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("흥부와 놀부들", "종이책", 3, "제비..", "까만 제비..", "김제비", 100, "목차입니다.", 20000, "2023-12-08");
